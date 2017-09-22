5918fc53862679bc4f90d528e4bba89d5b1ac139
Braden M. Kelley <redbmk@gmail.com>
2017-09-22 12:10:12 -0700

    add hot module replacement

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>
-, document.getElementById('root'));
+function render(Component) {
+  ReactDOM.render(
+    <MuiThemeProvider>
+      <Component />
+    </MuiThemeProvider>
+  , document.getElementById('root'));
+}
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(require('./App').default);
+  });
+}
+
+render(App);
+
 registerServiceWorker();
